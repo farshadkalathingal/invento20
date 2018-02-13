8fa617186d60eb0d44b5c077acab2745c64ce963
Biswas <biswasb007@gmail.com>
2018-02-13 17:36:19 +0530

    imageurl returns proper name

--- invento18/events/models.py
+++ invento18/events/models.py
@@ -22,4 +22,7 @@ class Event(models.Model):
 class ImageUrl(models.Model):
     name = models.CharField(max_length=50, blank=True)
     url = models.URLField()
-    event = models.ForeignKey(Event, related_name='image_url')
+    event = models.ForeignKey(Event, related_name='image_urls')
+
+    def __str__(self):
+        return self.name + " - " + self.event.title
