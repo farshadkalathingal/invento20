d57e600ccdc21e8d2048495be7f4430f41ca29a9
Biswas <biswasb007@gmail.com>
2018-02-15 07:40:38 +0530

    Removed unnecessary ContextMixin

--- invento18/events/views.py
+++ invento18/events/views.py
@@ -3,12 +3,6 @@ from django.views.generic import DetailView
 from django.views.generic.base import ContextMixin
 from .models import Event
 
-class EventContextMixin(ContextMixin):
-    def get_context_data(self, **kwargs):
-        context = super(EventDetailView, self).get_context_data(**kwargs)
-        context['image_urls'] = Event.image_urls.all()
-        return context
-
 class EventDetailView(DetailView):
     model = Event
     template_name = 'pages/event_detail.html'
