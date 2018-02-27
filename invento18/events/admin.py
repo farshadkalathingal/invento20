36a33d9b5fdafbf73177145dc0ac027ba27e50af
Biswas B <Biswasb007@gmail.com>
2018-02-27 20:35:44 +0530

    added filters to admin page

--- invento18/events/admin.py
+++ invento18/events/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Event, ImageUrl
 # Register your models here.
 
-@admin.register(Event, ImageUrl)
+@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'category', '_type', 'fee', 'prize', 'day')
+    list_filter = ('category', '_type', 'fee', 'prize')
