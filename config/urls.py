2d70709f3c84e8cecbd3ed9f060531715199d2dd
farshadkalathingal <farshadkalathingal@gmail.com>
2020-01-21 09:02:56 +0530

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,8 @@ from django.views.generic.base import RedirectView
 from django.views import defaults as default_views
 from rest_framework import routers
 
-from invento18.events.views import EventDetailView, departmentview
+
+from invento18.events.views import EventDetailView, departmentview, ambassodar_create_view, ambassador_page
 from invento18.events.serializers import EventList
 
 urlpatterns = [
@@ -17,6 +18,8 @@ urlpatterns = [
     url(r'^login/$', TemplateView.as_view(template_name='pages/login.html'), name='login'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     url(r'^3d/$', TemplateView.as_view(template_name='pages/3d.html'), name='3d'),
+    url(r'^campus/$', ambassador_page, name='ambassador'),
+    url(r'^register/$', ambassodar_create_view, name='campus_ambassador'),
     url(r'share/$', TemplateView.as_view(template_name='pages/ambassador.html'), name='share'),
     url(r'api/v1/events/(?P<category>[\w]{1,3})/(?P<_type>[\w]{0,3})', EventList.as_view()),
     url(r'^general/$', departmentview, name='general'),
